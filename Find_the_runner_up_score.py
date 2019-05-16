b2dcc095590eb38fa20ab1450335485e93dbed2d
John Pekcan <johnpekcan@gmail.com>
2019-05-16 02:10:21 -0700

    find the runner up score

---
+++ Find_the_runner_up_score.py
@@ -0,0 +1,16 @@
+number = int(input())
+array = map(int, input().split())
+answer = []
+
+if 2 <= number <= 10:
+   for arg in array:
+       if -100 <= arg <= 100:
+           if arg not in answer:
+               answer.append(arg)
+       else:
+           print("Scores need to be in between -100 and 100")
+else:
+    print("The number of score cards must be between 2 and 10")
+
+close = sorted(answer)
+print(close[-2])
